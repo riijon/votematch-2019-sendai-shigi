0654de4f77f558ff984257a6d4b46b50936e13a0
Riku Takenaka <riijon@gmail.com>
2019-07-30 01:26:27 +0900

    [update] 結果画面の仮データを作成

--- pages/candidate-answers.vue
+++ pages/candidate-answers.vue
@@ -1,25 +1,27 @@
 <template>
   <div>
-    <table>
-      <thead>
-      <tr>
-        <th>名前</th>
-        <th>ふりがな</th>
-        <th>Q1</th>
-        <th>Q2</th>
-        <th>Q3</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="candidate in candidates" :key="candidate.id">
-        <td>{{candidate.name}}</td>
-        <td>{{candidate.hurigana}}</td>
-        <td>{{candidate.q1}}</td>
-        <td>{{candidate.q2}}</td>
-        <td>{{candidate.q3}}</td>
-      </tr>
-      </tbody>
-    </table>
+    <template v-for="idea in ideaAnswers">
+      <h4 :key="idea.title">{{idea.title}}</h4>
+      <el-table
+        :data="idea.answers"
+        style="width: 100%"
+      >
+        <el-table-column
+          fixed
+          prop="question"
+          label="質問"
+          width="150">
+        </el-table-column>
+        <el-table-column
+          fixed
+          v-for="candidate in candidates"
+          :key="candidate.id"
+          :prop="candidate.id.toString()"
+          :label="candidate.name"
+          width="100">
+        </el-table-column>
+      </el-table>
+    </template>
   </div>
 </template>
 
@@ -28,6 +30,48 @@
     asyncData({store}) {
       return {
         candidates: store.getters['candidates']
+      }
+    },
+    data() {
+      return {
+        ideaAnswers: [
+          {
+            title: '経済の活性化について',
+            answers: [
+              {
+                question: '福祉の充実、雇用対策など、行政主導で経済を活性化させる',
+                "1": '○',
+                "2": ''
+              }, {
+                question: '税は安く、厳しいルールは撤廃して、民間の自由な取引で経済を活性化させる',
+                "1": '',
+                "2": '○'
+              }, {
+                question: 'どちらとも言えない',
+                "1": '',
+                "2": ''
+              }
+            ]
+          },
+          {
+            title: '男女の権利について',
+            answers: [
+              {
+                question: '男性も女性も、まったく同じ権利を得るべき',
+                "1": '○',
+                "2": '○'
+              }, {
+                question: '性差に合わせた、柔軟な権利を得るべき',
+                "1": '',
+                "2": ''
+              }, {
+                question: 'どちらとも言えない',
+                "1": '',
+                "2": ''
+              }
+            ]
+          }
+        ]
       }
     },
   }
