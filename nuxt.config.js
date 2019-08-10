494216e6e0f8bcf7f4ea82b027f6f7b59e5e0d8f
Riku Takenaka <riijon@gmail.com>
2019-08-10 18:42:47 +0900

    [update] font awesomeの導入とmetaの設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,27 @@
-
 export default {
   mode: 'universal',
   /*
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    htmlAttrs: {
+      prefix: 'og: http://ogp.me/ns#'
+    },
+    titleTemplate: '%s - 仙台市議選ボートマッチ2019',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: '仙台市議選ボートマッチ2019は、有権者（あなた）と立候補者の双方に同内容のアンケートに答えてもらい、政策についての考えのマッチ率（一致度）を測定することができるインターネットサービスです。' },
+      { hid: 'og:site_name', property: 'og:site_name', content: '仙台市議選ボートマッチ2019' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://example.com' },
+      { hid: 'og:title', property: 'og:title', content: '仙台市議選ボートマッチ2019' },
+      { hid: 'og:description', property: 'og:description', content: '仙台市議選ボートマッチ2019は、有権者（あなた）と立候補者の双方に同内容のアンケートに答えてもらい、政策についての考えのマッチ率（一致度）を測定することができるインターネットサービスです。' },
+      { hid: 'og:image', property: 'og:image', content: 'https://example.com/img/ogp/common.jpg' },
+      // { property: 'article:publisher', content: 'FacebookURL' },
+      // { property: 'fb:app_id', content: 'FacebookAppID' },
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:site', content: '@mediage_info' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -18,7 +30,7 @@ export default {
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: { color: '#cccccc' },
   /*
   ** Global CSS
   */
@@ -41,6 +53,14 @@ export default {
     'nuxt-webfontloader',
     // 'nuxt-basic-auth-module',
     'vue-scrollto/nuxt',
+    ['nuxt-fontawesome', {
+      imports: [
+        {
+          set: '@fortawesome/free-solid-svg-icons',
+          icons: ['faPaperPlane', 'faCheck', 'faCalculator', 'faAdjust', 'faArchive']
+        }
+      ]
+    }],
   ],
 
   basic: {
