96ee222c6c9e18ed2c84f1f521a115960799d4ab
Riku Takenaka <riijon@gmail.com>
2019-08-20 18:12:19 +0900

    [update] og imageの追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
       { hid: 'og:url', property: 'og:url', content: 'https://mediage.org/votematch/sen2019' },
       { hid: 'og:title', property: 'og:title', content: '仙台市議選ボートマッチ2019' },
       { hid: 'og:description', property: 'og:description', content: '仙台市議選ボートマッチ2019は、有権者（あなた）と立候補者の双方に同内容のアンケートに答えてもらい、政策についての考えのマッチ率（一致度）を測定することができるインターネットサービスです。' },
-      { hid: 'og:image', property: 'og:image', content: 'https://example.com/img/ogp/common.jpg' },
+      { hid: 'og:image', property: 'og:image', content: 'https://mediage.org/votematch/sen2019/images/og-image.jpg' },
       // { property: 'article:publisher', content: 'FacebookURL' },
       // { property: 'fb:app_id', content: 'FacebookAppID' },
       { name: 'twitter:card', content: 'summary' },
